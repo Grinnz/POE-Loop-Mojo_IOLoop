509f59c47a97bab8f439802202e897b0997eedda
Dan Book <grinnz@grinnz.com>
2015-01-10 16:50:24 -0500

    include EV tests in automated testing

--- lib/POE/Loop/Mojo_IOLoop_Tester_EV.pm
+++ lib/POE/Loop/Mojo_IOLoop_Tester_EV.pm
@@ -2,12 +2,13 @@
 BEGIN { $ENV{POE_EVENT_LOOP} = 'POE::Loop::Mojo_IOLoop' }
 BEGIN { $ENV{MOJO_REACTOR} = 'Mojo::Reactor::EV' }
 sub skip_tests {
-	return "tests for author testing only" unless $ENV{AUTHOR_TESTING};
+	return "tests for author testing only"
+		unless $ENV{AUTHOR_TESTING} or $ENV{AUTOMATED_TESTING};
 	return "Mojo::IOLoop tests require the Mojo::IOLoop module" if (
 		do { eval "use Mojo::IOLoop"; $@ }
 	);
 	return "EV module required to test Mojo::Reactor::EV backend" if (
-		do { eval "use EV"; $@ }
+		do { eval "use EV 4.0"; $@ }
 	);
 	if (shift eq '00_info') {
 		my $reactor = Mojo::IOLoop->singleton->reactor;
